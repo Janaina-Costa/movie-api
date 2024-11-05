0d81352cc37d6b258e54cf8d8e2f8b664d8a2422
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-05 03:00:17 -0300

    feat: objeto de valor movieName

---
+++ src/domain/core/src/shared/valueObject/MovieName.ts
@@ -0,0 +1,11 @@
+import SimpleText from "./SimpleText";
+
+
+export default class MovieName extends SimpleText{
+  constructor(value:string, min:number = 1, max:number = 120){
+    super(value, min, max)
+    if (!value.match(/^[a-zA-ZÀ-ÿ0-9&\s]+$/)) {
+      throw new Error('Movie name contains invalid characters');
+    }
+  }
+}
