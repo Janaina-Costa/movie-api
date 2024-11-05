5c54c0f081277b763172f40726e34347589b7590
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-05 02:59:34 -0300

    feat: objeto de valor entity

---
+++ src/domain/core/src/shared/valueObject/Entity.ts
@@ -0,0 +1,28 @@
+import Id from "./Id";
+
+export interface EntityProps {
+  id?: string;
+  props?: any;
+}
+
+export default abstract class Entity<T, Props extends EntityProps> {
+  readonly id: Id;
+  readonly props: Props;
+
+  constructor(props: Props ) {
+    this.id = new Id(props?.id);
+    this.props = { ...props, id: this.id.value };
+  }
+
+  isSameId(entidade: Entity<T, Props>): boolean {
+    return this.id?.value === entidade.id?.value;
+  }
+
+  isDifferentId(entidade: Entity<T, Props>): boolean {
+    return !this.isSameId(entidade);
+  }
+
+  clone(newProps: Props): T {
+    return new (this.constructor as any)({ ...this.props, ...newProps });
+  }
+}
