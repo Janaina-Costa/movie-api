8b9ee14816137d8afcf5779b3fa92ad85dea51ed
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-06 08:39:12 -0300

    feat: add método para validação de id

--- src/domain/core/src/shared/valueObject/Id.ts
+++ src/domain/core/src/shared/valueObject/Id.ts
@@ -1,14 +1,17 @@
-import {v4 as uuid, validate} from 'uuid'
+import { v4 as uuid, validate } from "uuid";
 
+export default class Id {
+  readonly value: string;
 
-export default class Id{
-    readonly value: string
+  constructor(value?: string) {
+    this.value = value ?? uuid();
 
-    constructor(value?:string){
-       this.value = value ?? uuid()
-
-        if(!validate(this.value)){
-            throw new Error(`Invalid id: ${value}`)
-        }
+    if (!Id.isValido(this.value)) {
+      throw new Error(`Invalid id: ${value}`);
     }
+  }
+
+  static isValido(id: string): boolean {
+    return validate(id);
+  }
 }
