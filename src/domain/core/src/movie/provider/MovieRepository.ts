d16fab4b3ca728b872d285f85358be6e919e4485
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-06 20:17:49 -0300

    fix: arruma tipagem

--- src/domain/core/src/movie/provider/MovieRepository.ts
+++ src/domain/core/src/movie/provider/MovieRepository.ts
@@ -1,9 +1,9 @@
 import Movie from "../model/Movie";
 
-export interface MovieRepository{
+export interface MovieRepository {
   save(movie: Movie): Promise<void>;
-  findMyId(id: string): Promise<Movie>;
-  findByName(name: string): Promise<Movie>;
-  findAll(): Promise<Movie[]>;
-  delete(id: string): Promise<void>;
+  findMyId(id: string): Promise<Movie | null>;
+  findByName(name: string): Promise<Movie | null>;
+  findAll(): Promise<Movie[] | []>;
+  delete(id: string): Promise<void | null>;
 }
