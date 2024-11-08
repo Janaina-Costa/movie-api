26c2c34efde19cc2989ae7b5d81d24c017352928
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-08 17:55:03 -0300

    feat: cria facade do movie e seus métodos

--- src/adapters/facade/MovieFacade.ts
+++ src/adapters/facade/MovieFacade.ts
@@ -1,8 +1,15 @@
-import { FindMovies, MovieRepository, SaveMovie } from "@/domain/core/src";
+import {
+  FindMovies,
+  MovieRepository,
+  SaveMovie,
+  FindMovieByName,
+  FindMovieById,
+} from "@/domain/core/src";
 import { MovieDTO } from "../DTO/MovieDTO";
+import UpdateMovie from "@/domain/core/src/movie/service/UpdateMovie";
 
 export default class MovieFacade {
-  constructor(private movieRepository: MovieRepository) {}
+  constructor(private readonly movieRepository: MovieRepository) {}
 
   async save(dto: MovieDTO): Promise<void> {
     const useCase = new SaveMovie(this.movieRepository);
@@ -25,5 +32,35 @@ export default class MovieFacade {
     const movie = await useCase.execute();
 
     return movie.map((m) => m.props);
+  }
+
+  async showById(id: string): Promise<MovieDTO | null> {
+    const useCase = new FindMovieById(this.movieRepository);
+    const movie = await useCase.execute(id);
+    return movie ? movie.props : null;
+  }
+
+  async showByName(name: string): Promise<MovieDTO | null> {
+    const useCase = new FindMovieByName(this.movieRepository);
+    const movie = await useCase.execute(name);
+
+    return movie ? movie.props : null;
+  }
+
+  async update(id: string, dto: MovieDTO): Promise<MovieDTO | null> {
+    const useCase = new UpdateMovie(this.movieRepository);
+    const movie = await useCase.execute({
+      id,
+      name: dto.name!,
+      image: dto.image!,
+      genre: dto.genre!,
+      linkUrl: dto.linkUrl!,
+      watchedDate: dto.watchedDate!,
+      userOpinion: dto.userOpinion!,
+      review: dto.review!,
+      isFirstTimeWatching: dto.isFirstTimeWatching!,
+      quantityViews: dto.quantityViews!,
+    });
+    return movie ? movie.props : null;
   }
 }
