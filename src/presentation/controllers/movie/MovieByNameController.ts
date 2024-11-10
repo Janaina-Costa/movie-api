f0230d9e19f949da2190f825997228ca2d859d04
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-10 11:33:42 -0300

    feat: busca pelo nome

---
+++ src/presentation/controllers/movie/MovieByNameController.ts
@@ -0,0 +1,16 @@
+import CoreFaced from "@/adapters/facade";
+import { Request, Response } from "express";
+
+export default class MovieByNameController {
+  constructor() {}
+
+  render = async (req: Request, res: Response) => {
+    try {
+      const { name } = req.body;
+
+      const movieExists = await CoreFaced.movie.showByName(name);
+    } catch (e: any) {
+      return res.status(500).json({ message: e.message });
+    }
+  };
+}
