c6b6b613ed5c4346fa58d0e6c5087e812c9d12ba
Janaina-Costa <janainacosta.dev@gmail.com>
2024-11-16 23:51:11 -0300

    feat: finaliza metodos do repository sql

--- src/infrastructure/repositories/MovieRepositorySQL.ts
+++ src/infrastructure/repositories/MovieRepositorySQL.ts
@@ -1,29 +1,62 @@
 import { Movie, MovieRepository } from "@/domain/core/src";
-import { connectDataBase } from "../database/sqlServer/dbsql";
 import query from "../database/sqlServer/queryDb";
-import { DB_SQL } from "../database/sqlServer";
+import connectDataBase from "../database/sqlServer/dbSql";
+import { MovieProps } from "@/domain/core/src/movie/model/Movie";
+import { MovieDTO } from "@/adapters/DTO/MovieDTO";
 
 export default class MovieRepositorySQL implements MovieRepository {
-  findMyId(id: string): Promise<Movie | null> {
-    throw new Error("Method not implemented.");
+  async findMyId(id: string): Promise<Movie | null> {
+    const pool = await connectDataBase();
+    const movie = await pool.request().input("id", id).query(query.findById);
+    return movie.recordset[0] ? new Movie({ ...movie.recordset[0] }) : null;
   }
+
   async findByName(name: string): Promise<Movie | null> {
     const pool = await connectDataBase();
     const movie = await pool.request().input("name", name).query(query.findOne);
-    return movie.recordset[0];
+    return movie.recordset[0] ?? null;
   }
-  findAll(): Promise<Movie[] | []> {
-    throw new Error("Method not implemented.");
+
+  async findAll(): Promise<Movie[] | []> {
+    const pool = await connectDataBase();
+    const movies = await pool.request().query(query.findAll);
+    return movies.recordset
+      ? movies.recordset.map((movie: MovieProps) => new Movie({ ...movie }))
+      : [];
   }
-  update(id: string, movie: Movie): Promise<Movie | null> {
-    throw new Error("Method not implemented.");
+
+  async update(id: string, movie: Movie): Promise<Movie | null> {
+    const pool = await connectDataBase();
+    const movieDB: MovieDTO = {
+      id: id,
+      name: movie.name.value,
+      image: movie.image.value,
+      genre: movie.genre.genre,
+      linkUrl: movie.linkUrl.value,
+      watchedDate: movie.watchedDate.value,
+      userOpinion: movie.userOpinion.value,
+      review: movie.review.review,
+      isFirstTimeWatching: movie.isFirstTimeWatching,
+      quantityViews: movie.quantityViews.value,
+    };
+
+    const request = pool.request();
+    for (const [key, value] of Object.entries(movieDB)) {
+      request.input(key, value);
+    }
+
+    const result = await request.query(query.update);
+
+    return result.rowsAffected[0] > 0 ? new Movie({ ...movie.props }) : null;
   }
-  delete(id: string): Promise<void | null> {
-    throw new Error("Method not implemented.");
+
+  async delete(id: string): Promise<void | null> {
+    const pool = await connectDataBase();
+    await pool.request().input("id", id).query(query.delete);
   }
 
   async save(movie: Movie): Promise<void> {
-    const movieDB: any = {
+    const movieDB: MovieDTO = {
       id: movie.id.value,
       name: movie.name.value,
       image: movie.image.value,
@@ -36,18 +69,10 @@ export default class MovieRepositorySQL implements MovieRepository {
       quantityViews: movie.quantityViews.value,
     };
     const pool = await connectDataBase();
-    await pool
-      .request()
-      .input("id", DB_SQL.VarChar, movieDB.id)
-      .input("name", DB_SQL.VarChar, movieDB.name)
-      .input("image", DB_SQL.VarChar, movieDB.image)
-      .input("genre", DB_SQL.VarChar, movieDB.genre)
-      .input("linkUrl", DB_SQL.VarChar, movieDB.linkUrl)
-      .input("watchedDate", DB_SQL.VarChar, movieDB.watchedDate)
-      .input("userOpinion", DB_SQL.Text, movieDB.userOpinion)
-      .input("review", DB_SQL.VarChar, movieDB.review)
-      .input("isFirstTimeWatching", DB_SQL.Bit, movieDB.isFirstTimeWatching)
-      .input("quantityViews", DB_SQL.Int, movieDB.quantityViews)
-      .query(query.create);
+    const request = pool.request();
+    for (const [key, value] of Object.entries(movieDB)) {
+      request.input(key, value);
+    }
+    await request.query(query.create);
   }
 }
